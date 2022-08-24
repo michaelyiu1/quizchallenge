7a393e30005f01defdd5cfd2d54db412f870d9c4
Michael Yiu <maplepie32@gmail.com>
2022-08-23 18:12:46 -0700

    User can take the quiz, view the previous score, and play again

--- script.js
+++ script.js
@@ -7,26 +7,33 @@ let multipleChoiceButton = document.querySelectorAll(".multiple-choice-button");
 let quizQuestion = document.querySelector(".quiz-question");
 let enterInitials = document.querySelector("#initials");
 let submit = document.querySelector("#submit");
+let scoresTable = document.querySelector(".scores-table");
+let playAgain = document.querySelector(".play-again");
+let viewScores = document.querySelector(".view-scores");
+let previousScore = document.querySelector(".previous-score");
 
 //Setting the multiple choice options and to initials input hidden and initializing iterators
 multipleChoice.style.visibility = "hidden";
 enterInitials.style.visibility = "hidden";
 submit.style.visibility = "hidden";
+playAgain.style.visibility = "hidden";
+scoresTable.style.visibility = "hidden";
 
 let qIterator = 0;
 let setAnswer = 0;
 let userScore = 0;
+let scoreNumber = 0;
+let scoreArray = [];
 
 
 //Array for Question Numbers
-let qNumber = ["Question 1", "Question 2","Question 3", "Enter your Initials"];
+let qNumber = ["Question 1", "Question 2","Question 3"];
 
 //Array for Quiz Questions
 let qList = [
     "What is the largest mammel?",
     "What do you get when you cross a hippo and a walrus?",
     "How big is the smallest tortuga?",
-    "Thank you for playing"
 ];
 
 //Setting the Multiple Choice options for each questions
@@ -42,8 +49,15 @@ answers = ["", "Whale","Hippo Walrus", "75 pounds"];
 //Function for starting the quiz when user clicks "start quiz"
 function quiz(){
 
-    startQuiz.style.display = "none";
+    let qIterator = 0;
+    let setAnswer = 0;
+    let userScore = 0;
+    playAgain.style.visibility = "hidden";
+    startQuiz.style.visibility = "hidden";
+    scoresTable.style.visibility = "hidden";
     multipleChoice.style.visibility = "visible";
+    secondsLeft.textContent = "30";
+
     quizNumber.textContent = qNumber[qIterator];
     quizQuestion.textContent = qList[qIterator];
 
@@ -52,14 +66,21 @@ function quiz(){
     multipleChoice.children[2].textContent = c[setAnswer];
     multipleChoice.children[3].textContent = d[setAnswer];
 
+
+    //Setting a timer for the quiz
     var timer = setInterval(function() {
         let time = secondsLeft.textContent;
         
-        if(time>1){
+        if(time>1 && qIterator < 3){
             secondsLeft.textContent--;
         } else{
             secondsLeft.textContent--;
             clearInterval(timer);
+            quizNumber.textContent = "Thank you for Playing";
+            quizQuestion.textContent = "enter your initials";
+            userInitials();
+            secondsLeft.textContent = "0";
+
         }
         }, 1000)
 }
@@ -87,7 +108,6 @@ function nextquestion(){
         userInitials();
         qIterator = 0;
     }
-
 }
 
 //Function that checks the user the user selected and adds points if it is correct
@@ -99,25 +119,55 @@ function checkAnswer(userAnswer){
 
     } else {
         console.log("wrong answer");
+        secondsLeft.textContent = secondsLeft.textContent -10;
     }
-
 }
 
 function userInitials(){
+    quizNumber.textContent = "Thank you for Playing";
+    quizQuestion.textContent = "enter your initials";
     multipleChoice.style.visibility = "hidden";
     enterInitials.style.visibility = "visible";
     submit.style.visibility = "visible";
-
 }
 
-function highScores(){
-    let initials = enterInitials.value;
-    console.log(initials);
+function highScores(event){
+    event.preventDefault();
+
+    playAgain.style.visibility = "visible";
+    scoresTable.style.visibility = "visible";
+
+
+    localStorage.setItem("score", enterInitials.value + " scored: " + userScore);
+    let score = localStorage.getItem('score');
+    enterInitials.style.visibility = "hidden";
+    submit.style.visibility = "hidden";
+
+    previousScore.textContent = score;
+    userScore = 0;
+
+    scoreNumber++;
+}
+
+function viewHighScores(){
     
+    if(viewScores.textContent === "View High Scores"){
+        viewScores.textContent = "Hide Scores";
+        scoresTable.style.visibility = "visible";
+    } else {
+        viewScores.textContent = "View High Scores";
+        scoresTable.style.visibility = "hidden";
+    }
+
+    let score = localStorage.getItem('score');
+    previousScore.textContent = score;
 }
+
 
 //Event listeners
 startQuiz.addEventListener("click", quiz);
+playAgain.addEventListener("click",quiz);
+viewScores.addEventListener("click",viewHighScores);
 
 for (i=0; i<multipleChoiceButton.length; i++){
     multipleChoiceButton[i].addEventListener("click",nextquestion);
